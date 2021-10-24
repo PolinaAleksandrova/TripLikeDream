6b393499b9246124478266d13525d95caf7f3e76
yevhenii.bezpalchenko <yevhenii.bezpalchenko@nure.ua>
2021-10-24 13:18:55 +0300

    1 dev commit

--- TreapLikeDream/app.js
+++ TreapLikeDream/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "pug");
 
 app.use("/main", function(request, response){
-    response.render("index", {})
+    response.render("index", {title:"main"})
 });
 app.use("/", function(request, response){
     response.status(404).render("error404", {});
