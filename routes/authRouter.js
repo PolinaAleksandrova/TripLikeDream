b575d7a776642ec5d6dc924ea724974dfd04347c
artemandriychenko@gmail.com <artemandriychenko@gmail.com>
2021-11-28 18:13:03 +0200

    auth logic

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,7 +7,7 @@ const roleMiddleware = require('../middlewaree/roleMiddleware')
 
 router.post('/registration', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
+    check('password', "Пароль должен быть больше 4 и меньше 11 символов").isLength({min:4, max:10})
 ], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', roleMiddleware(['ADMIN']), controller.getUsers)
