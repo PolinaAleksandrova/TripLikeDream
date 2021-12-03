693ca01ec9e98b280bd8136553128a9acd21c9b1
artemandriychenko@gmail.com <artemandriychenko@gmail.com>
2021-12-03 15:31:39 +0200

    auth and registr form

--- routes/authController.js
+++ routes/authController.js
@@ -16,20 +16,22 @@ const generateAccessToken = (id, roles) => {
 class authController {
     async registration(req, res) {
         try {
+            console.log(req)
             const errors = validationResult(req)
             if (!errors.isEmpty()) {
-                return res.status(400).json({message: "Ошибка при регистрации", errors})
+                return "Ошибка при регистрации"
             }
-            const {username, password} = req.body;
+            const username = req.body.login
+            const password = req.body.password
             const candidate = await User.findOne({username})
             if (candidate) {
-                return res.status(400).json({message: "Пользователь с таким именем уже существует"})
+                return "Пользователь с таким именем уже существует"
             }
             const hashPassword = bcrypt.hashSync(password, 7);
             const userRole = await Role.findOne({value: "USER"})
             const user = new User({username, password: hashPassword, roles: [userRole.value]})
             await user.save()
-            return res.json({message: "Пользователь успешно зарегистрирован"})
+            return "Пользователь успешно зарегистрирован"
         } catch (e) {
             console.log(e)
             res.status(400).json({message: 'Registration error'})
@@ -38,17 +40,19 @@ class authController {
 
     async login(req, res) {
         try {
-            const {username, password} = req.body
+            const username = req.body.login
+            const password = req.body.password
             const user = await User.findOne({username})
             if (!user) {
-                return res.status(400).json({message: `Пользователь ${username} не найден`})
+                return `Пользователь ${username} не найден`
             }
             const validPassword = bcrypt.compareSync(password, user.password)
             if (!validPassword) {
-                return res.status(400).json({message: `Введен неверный пароль`})
+                return "Введен неверный пароль"
             }
             const token = generateAccessToken(user._id, user.roles)
-            return res.json({token})
+            return "1"
+            //return res.json({token})
         } catch (e) {
             console.log(e)
             res.status(400).json({message: 'Login error'})
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,11 +5,31 @@ const {check} = require("express-validator")
 const authMiddleware = require('../middlewaree/authMiddleware')
 const roleMiddleware = require('../middlewaree/roleMiddleware')
 
-router.post('/registration', [
+router.get('/registration', async (req, res) => {
+    res.render('registration', {});
+})
+router.post('/registration', async (req, res)=>{
+    const err = await controller.registration(req, res) 
+    if(err == "Пользователь успешно зарегистрирован"){
+        res.redirect('/auth/login')
+    }
+    res.render('registration', {err});
+})
+/*router.post('/registration', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
     check('password', "Пароль должен быть больше 4 и меньше 11 символов").isLength({min:4, max:10})
-], controller.registration)
-router.post('/login', controller.login)
+], controller.registration)*/
+router.get('/login', async (req, res) => {
+    res.render('login', {});
+})
+//router.post('/login', controller.login)
+router.post('/login', async (req, res)=>{
+    const err = await controller.login(req, res) 
+    if(err == 1){
+        res.redirect('../../')
+    }
+    res.render('login', {err});
+})
 router.get('/users', roleMiddleware(['ADMIN']), controller.getUsers)
 
 module.exports = router
